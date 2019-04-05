35d4741afb4e6bf7716d198bb7f54f0e2bd41240
wingytsui8 <tsuiwingyan@gmail.com>
2019-04-05 17:42:27 +0800

    init

---
+++ zyro/index.php
@@ -0,0 +1,79 @@
+<?php
+	error_reporting(E_ALL); @ini_set('display_errors', true);
+	@session_start();
+	$pages = array(
+		'0'	=> array('id' => '1', 'alias' => '', 'file' => '1.php','controllers' => array()),
+		'1'	=> array('id' => '2', 'alias' => 'About-us', 'file' => '2.php','controllers' => array()),
+		'2'	=> array('id' => '3', 'alias' => 'Contacts', 'file' => '3.php','controllers' => array())
+	);
+	$forms = array(
+		'3'	=> array(
+			'd251a956' => Array( 'email' => '', 'emailFrom' => '', 'subject' => 'Inquiry from the web page', 'sentMessage' => 'Form was sent.', 'object' => '', 'objectRenderer' => '', 'loggingHandler' => '', 'smtpEnable' => false, 'smtpHost' => null, 'smtpPort' => null, 'smtpEncryption' => null, 'smtpUsername' => null, 'smtpPassword' => null, 'recSiteKey' => null, 'recSecretKey' => null, 'maxFileSizeTotal' => null, 'fields' => array( array( 'fidx' => '0', 'name' => 'Name', 'type' => 'input', 'options' => '' ), array( 'fidx' => '1', 'name' => 'E-mail', 'type' => 'input', 'options' => '' ), array( 'fidx' => '2', 'name' => 'Message', 'type' => 'textarea', 'options' => '' ) ) )
+		)
+	);
+	$langs = null;
+	$def_lang = null;
+	$base_lang = 'en';
+	$site_id = "513ae286";
+	$base_dir = dirname(__FILE__);
+	$base_url = '/';
+	$user_domain = 'tencenium.000webhostapp.com';
+	$show_comments = false;
+	require_once dirname(__FILE__).'/functions.inc.php';
+	$home_page = '1';
+	list($page_id, $lang, $urlArgs, $route) = parse_uri();
+	$user_key = "pN5dzFB1/38jRiQfAVGuvF/SaITA3svZSXKw";
+	$user_hash = "b1cb579a1bb9ef01";
+	$comment_callback = "http://us.zyro.com/comment_callback/";
+	$preview = false;
+	$mod_rewrite = true;
+	$page = isset($pages[$page_id]) ? $pages[$page_id] : null;
+	$hr_out = '';
+	if (!is_null($page)) {
+		handleComments($page['id']);
+		if (isset($_POST["wb_form_id"])) handleForms($page['id']);
+	}
+	ob_start();
+	if ($page) {
+		$fl = dirname(__FILE__).'/'.$page['file'];
+		if (is_file($fl)) {
+			ob_start();
+			include $fl;
+			$out = ob_get_clean();
+			$ga_out = '';
+			if ($lang && $langs) {
+				foreach ($langs as $ln => $default) {
+					$pageUri = getPageUri($page['id'], $ln);
+					$out = str_replace(urlencode('{{lang_'.$ln.'}}'), $pageUri, $out);
+				}
+			}
+			if (is_file($ga_file = dirname(__FILE__).'/ga_code') && $ga_code = file_get_contents($ga_file)) {
+				$ga_out = str_replace('{{ga_code}}', $ga_code, file_get_contents(dirname(__FILE__).'/ga.html'));
+			}
+			$out = str_replace('{{ga_code}}', $ga_out, $out);
+			$baseUrl = (isHttps() ? 'https' : 'http').'://'.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost').'/';
+			$out = str_replace('{{base_url}}', $baseUrl, $out);
+			$out = str_replace('{{curr_url}}', $baseUrl.($lang && $lang != $def_lang ? $lang.'/' : '').$route, $out);
+			$out = str_replace('{{hr_out}}', $hr_out, $out);
+			header('Content-type: text/html; charset=utf-8', true);
+			echo $out;
+		}
+	} else {
+		header("Content-type: text/html; charset=utf-8", true, 404);
+		if (is_file(dirname(__FILE__).'/404.html')) {
+			include '404.html';
+		} else {
+			echo "<!DOCTYPE html>\n";
+			echo "<html>\n";
+			echo "<head>\n";
+			echo "<title>404 Not found</title>\n";
+			echo "</head>\n";
+			echo "<body>\n";
+			echo "404 Not found\n";
+			echo "</body>\n";
+			echo "</html>";
+		}
+	}
+	ob_end_flush();
+
+?>
